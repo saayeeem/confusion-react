81e36f945ac6f75fe1cf09602a1b7b995d3ce0a1
saayeeem <chysayem9@gmail.com>
2021-08-02 21:20:38 +0600

    Presentational and Container Component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
+
 
 class App extends Component {
 
-constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+// constructor(props) {
+//     super(props);
+//     this.state = {
+//       dishes: DISHES,
+//       comments: DISHES.comments
+//     };
+//   }
 
 
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
+        {/* <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>
-       <Menu dishes={this.state.dishes} />
+        </Navbar> */}
+       <Main/>
 
       </div>
     );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,76 +1,20 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap'; 
-// class Menu extends Component {
-//     constructor(props) {
-//         super(props);
-  
-//     }
-//     render() {
-//         const menu = this.props.dishes.map((dish) => {
-//             return (
-//               <div key={dish.id} className="col-12 mt-5">
-//                 <Media tag="li">
-//                   <Media left middle>
-//                       <Media object src={dish.image} alt={dish.name} />
-//                   </Media>
-//                   <Media body className="ml-5">
-//                     <Media heading>{dish.name}</Media>
-//                     <p>{dish.description}</p>
-//                   </Media>
-//                 </Media>
-//               </div>
-//             );
-//         });
- 
-//         return (
-//           <div className="container">
-//             <div className="row">
-//               <Media list>
-//                   {menu}
-//               </Media>
-//             </div>
-//           </div>
-//         );
-//     }
-// }
+    CardTitle
+} from 'reactstrap';
+
+
 class Menu extends Component {
- 
     constructor(props) {
         super(props);
- 
-        this.state = {
-            selectedDish: null
-        }
-    }
- 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
- 
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
- 
+  
+  }
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
+              <div className="col-12 col-md-5 m-1">
                 <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
+                        onClick={() => this.props.onClick(dish.id)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -85,15 +29,11 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
                 </div>
-            </div>
         );
     }
 }
+
 
  
 export default Menu;
